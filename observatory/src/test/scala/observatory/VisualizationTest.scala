0987bc01093d78066f3f584658098318724145a1
Stephen Pascoe <stephen.pascoe@lirico.co.uk>
2017-04-27 17:51:27 +0100

    Test recursive spatial interpolation

--- observatory/src/test/scala/observatory/VisualizationTest.scala
+++ observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -19,6 +19,15 @@ class VisualizationTest extends FunSuite with GeneratorDrivenPropertyChecks {
     lon <- Gen.choose(-180.0, 180.0)
   } yield Location(lat, lon)
 
+  val sampleGen = {
+    val gen = for {
+      loc <- locationGen
+      value <- Gen.choose(-50.0, 50.0)
+    } yield (loc, value)
+
+    Gen.listOfN(10, gen)
+  }
+
   test("dist london->paris") {
     assert(Visualization.dist(london, paris) - 344.0 < 0.2)
     assert(Visualization.dist(london, paris) === Visualization.dist(paris, london))
@@ -33,6 +42,32 @@ class VisualizationTest extends FunSuite with GeneratorDrivenPropertyChecks {
 
   test("dist london->new york") {
     assert(Visualization.dist(london, newyork) - 5585.0 < 0.2)
+  }
+
+  test("Symetric idw") {
+    val sample: List[(Location, Double)] =
+      List((Location(30, 0), 5.0), (Location(0, 30), 5.0), (Location(-30, 0), 5.0), (Location(0, -30), 5.0))
+    val loc = Location(0, 0)
+
+    assert(Visualization.idw(sample, loc, Visualization.P) === 5.0)
+  }
+
+  test("At point idw") {
+    val sample: List[(Location, Double)] =
+      List((Location(30, 0), 10.0), (Location(0, 30), 5.0), (Location(-30, 0), 15.0), (Location(0, -30), 20.0))
+    val loc = Location(-30, 0)
+
+    assert(Visualization.idw(sample, loc, Visualization.P) === 15.0)
+  }
+
+  test("idw random samples") {
+    forAll (sampleGen, locationGen) { (sample: List[(Location, Double)], loc: Location) => {
+      val result = Visualization.idw(sample, loc, Visualization.P)
+      assert(result <= 50.0)
+      assert(result >= -50.0)
+    }
+
+    }
   }
 
 }
